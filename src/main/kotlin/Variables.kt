b9c1f9bd8345a73919086ddd0ef4eb9b289941de
Ephraim10 <ephraimmburu33@gmail.com>
2023-03-16 09:00:50 +0300

    First commit

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    var x:Int
+    var y:Float
+    var w:Double
+    var m:Char
+    var v:String
+    var j:Boolean
+    var answer:Double
+    var age:Int
+    var name:String
+
+    x =10
+    y =20.0f
+    w =30.0
+    m ='a'
+    v = "King"
+    j = true
+
+    answer = y + w
+    println(answer)
+    age = 100
+    name = "King"
+    println("Hello "+name+ ", you are "+age+" years old")
+    println("Hello $name, you are $age years old")
+
+    var firstNum =20
+    var secondNum =30
+    var jibu = firstNum + secondNum
+    println("Your answer is $jibu")
+
+}
---
+++ src/main/kotlin/VariablesTwo.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    var x:Int
+    val y:Int
+
+    x = 20
+    x = 30
+    println(x)
+
+    y =40
+    println(y)
+    var principle=10000
+    var rate =2.5
+    var time=5
+    var interest =( principle * rate * time)/100.0
+    println("Your interest is $interest")
+
+    // Calculating volume of a cylinder
+
+    var rad=21
+    var height=300
+    var volume =(22/7 * rad*rad *height)
+    println("Your volume is $volume")
+
+}
